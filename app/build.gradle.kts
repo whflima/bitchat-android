9bdfa31d5579b510da27207637603323914d00be
prudhvir3ddy <prudhvireddy.m01@gmail.com>
2025-07-11 17:15:54 +0530

    Upgrade compileSDK, kotlin, AGP, compose, core libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -39,9 +40,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
     packaging {
         resources {
